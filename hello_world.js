b8f31cfd25b4a07597da1bd498002c14173e5209
Rina168 <newmoon0829@gmail.com>
2022-12-16 15:15:56 +0800

    add moment

--- hello_world.js
+++ hello_world.js
@@ -1,5 +1,8 @@
 //console.log("hello, world!!!");
 
 var day = require('dayjs');
+var moment = require('moment'); 
 
 console.log(day().format('YYYY/MM/DD HH:mm:ss'));
+
+console.log(moment().format('dddd'));
